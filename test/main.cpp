ad15118cb19ea00ce1a6f2ca73c0c29cc7d279bb
luojiahua <luojiahua@luojiahuademacbook-air.local>
2016-09-07 12:57:29 +0800

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by luojiahua on 16/9/7.
+//  Copyright © 2016年 luojiahua. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
