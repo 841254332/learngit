264978c1642e72b72c2fbffe4efd8683675585a3
luojiahua <luojiahua@luojiahuadeMacBook-Air.local>
2016-09-09 17:07:01 +0800

    test

--- test/main.cpp
+++ test/main.cpp
@@ -8,11 +8,21 @@
 
 #include <iostream>
 using namespace std;
-void swap(int m, int n){
-    
+void swap(int &m, int &n){
+    int temp;
+    temp = m;
+    m = n;
+    n = temp;
 }
 int main() {
     // insert code here...
-    cout << "Hello, World!\n";
+    int a, b;
+    int *p1, *p2;
+    a = 1;
+    b = 2;
+    p1 = &a;
+    p2 = &b;
+    swap(a, b);
+    cout<<a<<","<<b<<endl;
     return 0;
 }
